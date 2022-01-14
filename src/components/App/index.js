e8bbd639fd937524f6208e79ca4741222ee2c1dc
Dovydas Lozys <reduktorius@gmail.com>
2022-01-14 11:33:10 +0000

    setting up the initial search page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return (<div>Hello World!</div>);
+  return (
+  <div>
+    <h2>Search GitHub username</h2>
+    <input type="text"></input>
+    <button>Search</button>
+  </div>);
 }
 
 export default App;
