628a09e4cb79ee3d67512d70ead6281e0584d73d
Karen Derbyshire <Karen@Thomass-Mac-mini.local>
2022-01-14 11:09:09 +0000

    plan made

---
+++ src/plan.js
@@ -0,0 +1,3 @@
+//  INITIAL SEARCH PAGE - Components: input, text, button.
+// USER DISPLAY - Component 1 Everything up to the Bio - links, profile picture, name(contact email).
+//Componment 2 - repositaries links 
